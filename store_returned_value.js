ee2848f2f818b7f59432ced5c0281da40874c555
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2023-02-13 11:37:25 +0200

    txtfield

---
+++ store_returned_value.js
@@ -0,0 +1,34 @@
+import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+// define the component
+export class NintexSampleTextfield extends LitElement {
+  //...
+  onChange(e) {
+    const args = {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+        // value coming from input change event. 
+        detail: e.target.value,
+    };
+    const event = new CustomEvent('ntx-value-change', args);
+    this.dispatchEvent(event);
+  }
+
+  
+  static getMetaConfig(){
+
+    return {
+      //...
+      properties: {
+        value: {
+          type: 'string',
+          title: 'Value',
+          isValueField: true,
+        },
+      },
+      events: ["ntx-value-change"],
+    };
+  }
+}
+//...
+
